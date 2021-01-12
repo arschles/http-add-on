582cfbf765597534b2e0a0b5fce11c16283821d0
Lucas Santos <lhs.santoss@gmail.com>
2021-01-12 18:50:26 -0300

    Move functions to other file

--- operator/controllers/app_objects.go
+++ operator/controllers/app_objects.go
@@ -2,14 +2,11 @@ package controllers
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/go-logr/logr"
 	"github.com/kedacore/http-add-on/operator/api/v1alpha1"
 	"github.com/kedacore/http-add-on/pkg/k8s"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/dynamic"
 	typedAppsv1 "k8s.io/client-go/kubernetes/typed/apps/v1"
 	typedCorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -47,6 +44,8 @@ func (rec *HTTPScaledObjectReconciler) removeAppObjects(
 	httpso *v1alpha1.HTTPScaledObject,
 ) error {
 	appName := httpso.Spec.AppName
+	interceptorName := fmt.Sprintf("%s-interceptor", appName)
+	externalScalerName := fmt.Sprintf("%s-ext-scaler", appName)
 
 	// set initial statuses
 	httpso.Status = v1alpha1.HTTPScaledObjectStatus{
@@ -59,6 +58,7 @@ func (rec *HTTPScaledObjectReconciler) removeAppObjects(
 	}
 	logger = rec.Log.WithValues("reconciler.appObjects", "removeObjects", "HTTPScaledObject.name", appName, "HTTPScaledObject.namespace", httpso.Namespace)
 
+	// Delete deploys
 	appsCl := rec.K8sCl.AppsV1().Deployments(req.Namespace)
 	if err := appsCl.Delete(appName, &metav1.DeleteOptions{}); err != nil {
 		logger.Error(err, "Deleting deployment")
@@ -67,13 +67,40 @@ func (rec *HTTPScaledObjectReconciler) removeAppObjects(
 	}
 	httpso.Status.DeploymentStatus = v1alpha1.Deleted
 
+	if err := appsCl.Delete(interceptorName, &metav1.DeleteOptions{}); err != nil {
+		logger.Error(err, "Deleting interceptor deployment")
+		httpso.Status.InterceptorStatus = v1alpha1.Error
+		return err
+	}
+
+	if err := appsCl.Delete(externalScalerName, &metav1.DeleteOptions{}); err != nil {
+		logger.Error(err, "Deleting external scaler deployment")
+		httpso.Status.ExternalScalerStatus = v1alpha1.Error
+		return err
+	}
+
+	// Delete Services
 	coreCl := rec.K8sCl.CoreV1().Services(req.Namespace)
 	if err := coreCl.Delete(appName, &metav1.DeleteOptions{}); err != nil {
-		logger.Error(err, "Deleting service")
+		logger.Error(err, "Deleting app service")
 		httpso.Status.ServiceStatus = v1alpha1.Error
 		return err
 	}
 	httpso.Status.ServiceStatus = v1alpha1.Deleted
+
+	if err := coreCl.Delete(interceptorName, &metav1.DeleteOptions{}); err != nil {
+		logger.Error(err, "Deleting interceptor service")
+		httpso.Status.InterceptorStatus = v1alpha1.Error
+		return err
+	}
+	httpso.Status.InterceptorStatus = v1alpha1.Deleted
+
+	if err := coreCl.Delete(externalScalerName, &metav1.DeleteOptions{}); err != nil {
+		logger.Error(err, "Deleting external scaler service")
+		httpso.Status.ExternalScalerStatus = v1alpha1.Error
+		return err
+	}
+	httpso.Status.ExternalScalerStatus = v1alpha1.Deleted
 
 	// TODO: use r.Client here, not the dynamic one
 	scaledObjectCl := k8s.NewScaledObjectClient(rec.K8sDynamicCl)
@@ -139,117 +166,5 @@ func (rec *HTTPScaledObjectReconciler) addAppObjects(
 		return err
 	}
 
-	return nil
-}
-
-func createScaledObject (
-	appInfo userApplicationInfo,
-	K8sDynamicCl dynamic.Interface,
-	logger logr.Logger,
-	httpso *v1alpha1.HTTPScaledObject,
-) error {
-	coreScaledObject := k8s.NewScaledObject(
-		appInfo.namespace,
-		appInfo.name,
-		appInfo.name,
-		fmt.Sprintf("%s.%s.svc.cluster.local:%d", appInfo.externalScalerName, appInfo.namespace, defaultExposedPort),
-	)
-	// TODO: use r.Client here, not the dynamic one
-	scaledObjectCl := k8s.NewScaledObjectClient(K8sDynamicCl)
-	if _, err := scaledObjectCl.
-		Namespace(appInfo.namespace).
-		Create(coreScaledObject, metav1.CreateOptions{}); err != nil {
-		logger.Error(err, "Creating ScaledObject")
-		httpso.Status.ScaledObjectStatus = v1alpha1.Error
-		return err
-	}
-	httpso.Status.ScaledObjectStatus = v1alpha1.Created
-	return nil
-}
-
-func createUserApp (
-	appInfo userApplicationInfo,
-	clients kubernetesClients,
-	logger logr.Logger,
-	httpso *v1alpha1.HTTPScaledObject,
-) error {
-	deployment := k8s.NewDeployment(appInfo.namespace, appInfo.name, appInfo.image, appInfo.port, []corev1.EnvVar{})
-	// TODO: watch the deployment until it reaches ready state
-	// Option: start the creation here and add another method to check if the resources are created
-	if _, err := clients.appsCl.Create(deployment); err != nil {
-		logger.Error(err, "Creating deployment")
-		httpso.Status.DeploymentStatus = v1alpha1.Error
-		return err
-	}
-	httpso.Status.DeploymentStatus = v1alpha1.Created
-
-	service := k8s.NewService(appInfo.namespace, appInfo.name, appInfo.port)
-	if _, err := clients.coreCl.Create(service); err != nil {
-		logger.Error(err, "Creating service")
-		httpso.Status.ServiceStatus = v1alpha1.Error
-		return err
-	}
-	httpso.Status.ServiceStatus = v1alpha1.Created
-	return nil
-}
-
-func createInterceptor (
-	appInfo userApplicationInfo,
-	clients kubernetesClients,
-	logger logr.Logger,
-	httpso *v1alpha1.HTTPScaledObject,
-) error {
-	interceptorEnvs := []corev1.EnvVar{
-		{
-			Name: "KEDA_HTTP_SERVICE_NAME",
-			Value: appInfo.name,
-		},
-		{
-			Name: "KEDA_HTTP_SERVICE_PORT",
-			Value: strconv.FormatInt(int64(appInfo.port), 10),
-		},
-	}
-
-	// NOTE: Interceptor port is fixed here because it's a fixed on the interceptor main (@see ../interceptor/main.go:49)
-	interceptorDeployment := k8s.NewDeployment(appInfo.namespace, appInfo.interceptorName, imageRegistry + interceptorImageName, int32(defaultExposedPort), interceptorEnvs)
-	if _, err := clients.appsCl.Create(interceptorDeployment); err != nil {
-		logger.Error(err, "Creating interceptor deployment")
-		httpso.Status.InterceptorStatus = v1alpha1.Error
-		return err
-	}
-
-	// NOTE: Interceptor port is fixed here because it's a fixed on the interceptor main (@see ../interceptor/main.go:49)
-	interceptorService := k8s.NewService(appInfo.namespace, appInfo.interceptorName, int32(defaultExposedPort))
-	if _, err := clients.coreCl.Create(interceptorService); err != nil {
-		logger.Error(err, "Creating interceptor service")
-		httpso.Status.InterceptorStatus = v1alpha1.Error
-		return err
-	}
-	httpso.Status.InterceptorStatus = v1alpha1.Created
-	return nil
-}
-
-func createExternalScaler (
-	appInfo userApplicationInfo,
-	clients kubernetesClients,
-	logger logr.Logger,
-	httpso *v1alpha1.HTTPScaledObject,
-) error {
-	// NOTE: Scaler port is fixed here because it's a fixed on the scaler main (@see ../scaler/main.go:17)
-	scalerDeployment := k8s.NewDeployment(appInfo.namespace, appInfo.externalScalerName, imageRegistry + externalScalerImageName, int32(defaultExposedPort), []corev1.EnvVar{})
-	if _, err := clients.appsCl.Create(scalerDeployment); err != nil {
-		logger.Error(err, "Creating scaler deployment")
-		httpso.Status.ExternalScalerStatus = v1alpha1.Error
-		return err
-	}
-
-	// NOTE: Scaler port is fixed here because it's a fixed on the scaler main (@see ../scaler/main.go:17)
-	scalerService := k8s.NewService(appInfo.namespace, appInfo.externalScalerName, int32(defaultExposedPort))
-	if _, err := clients.coreCl.Create(scalerService); err != nil {
-		logger.Error(err, "Creating scaler service")
-		httpso.Status.ExternalScalerStatus = v1alpha1.Error
-		return err
-	}
-	httpso.Status.ExternalScalerStatus = v1alpha1.Created
 	return nil
 }
